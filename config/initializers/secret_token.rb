c3ea794862341d55000d46dc7b306e20cba2be0b
saasbook <saasbook@saasbook.(none)>
2013-01-05 13:55:19 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mosta3mal::Application.config.secret_token = '8d42361bb55537e006055e6e078c316bb8af6859f302671364b476c6f3f62bae8ca425e2550322647f82a1e9aee86ffc94775eb0fd478abebac3680b5aa6ab46'
