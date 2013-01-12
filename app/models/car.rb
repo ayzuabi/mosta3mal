e2d5e24289e2de7677c0d108565ae2521dfc87dd
saasbook <saasbook@saasbook.(none)>
2013-01-12 12:38:16 -0800

    Home page, Owner Page, adding interface

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,11 @@
 class Car < ActiveRecord::Base
+  def self.all_models
+    %w(Matrix k5 sentra soul corolla-sedan tiida-hatchback elantra carens terios astra)
+  end
+
+  def self.all_makes
+    %w(hyundai kia nissan toyota daihatsu opel)
+  end
+
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,17 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
 cars = [{:make => 'Hyundai', :model => 'Matrix', :color => 'Silver', :type => 'Automatic', :year => '2005', :short_desc => 'Good Condition', :long_desc => 'only 45,000 km', :owner => 'Ayzuabi'},
-    	  {:make => '', :model => '', :color => '', :type => '', :year => '', :short_desc => '', :long_desc => '', :owner => ''},
-    	  {:make => '', :model => '', :color => '', :type => '', :year => '', :short_desc => '', :long_desc => '', :owner => ''},
-      	  {:make => '', :model => '', :color => '', :type => '', :year => '', :short_desc => '', :long_desc => '', :owner => ''},
-      	  {:make => '', :model => '', :color => '', :type => '', :year => '', :short_desc => '', :long_desc => '', :owner => ''},
-      	  {:make => '', :model => '', :color => '', :type => '', :year => '', :short_desc => '', :long_desc => '', :owner => ''},
-      	  {:make => '', :model => '', :color => '', :type => '', :year => '', :short_desc => '', :long_desc => '', :owner => ''},
-      	  {:make => '', :model => '', :color => '', :type => '', :year => '', :short_desc => '', :long_desc => '', :owner => ''},
-      	  {:make => '', :model => '', :color => '', :type => '', :year => '', :short_desc => '', :long_desc => '', :owner => ''},
-      	  {:make => '', :model => '', :color => '', :type => '', :year => '', :short_desc => '', :long_desc => '', :owner => ''},
-  	 ]
+{:make => 'kia', :model => 'k5', :color => 'black', :type => 'automatic', :year => '2010', :short_desc => 'good condition', :long_desc => '49,902 km', :owner => 'Ngharib'},
+{:make => 'nissan', :model => 'sentra', :color => 'black', :type => 'automatic', :year => '2005', :short_desc => 'good condition', :long_desc => '94,125 km', :owner => 'Ngharib'},
+{:make => 'kia', :model => 'soul', :color => 'silver', :type => 'automatic', :year => '2009', :short_desc => 'good condition', :long_desc => '38,000 km', :owner => 'Ayzuabi'},
+{:make => 'toyota', :model => 'corolla-sedan', :color => 'white', :type => 'automatic', :year => '2001', :short_desc => 'good condition', :long_desc => '105,731 km', :owner => 'Ngharib'},
+{:make => 'nissan', :model => 'tiida-hatchback', :color => 'black', :type => 'automatic', :year => '2005', :short_desc => 'good condition', :long_desc => '39,700 km', :owner => 'Ayzuabi'},
+{:make => 'hyundai', :model => 'elantra', :color => 'Burgundy', :type => 'automatic', :year => '2004', :short_desc => 'good condition', :long_desc => '20,834 km', :owner => 'Ngarib'},
+{:make => 'kia', :model => 'carens', :color => 'black', :type => 'automatic', :year => '2002', :short_desc => 'good condition', :long_desc => '83,000 km', :owner => 'Ayzuabi'},
+{:make => 'daihatsu', :model => 'terios', :color => 'red', :type => 'automatic', :year => '1997', :short_desc => 'good condition', :long_desc => '65,510 km', :owner => 'Ayzuabi'},
+{:make => 'opel', :model => 'astra', :color => 'silver', :type => 'automatic', :year => '1999', :short_desc => 'good condition', :long_desc => '93,096', :owner => 'Ngharib'},
+]
 
 cars.each do |car|
   Car.create!(car)
